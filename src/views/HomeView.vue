433a0734f19b7bea140305f55c2574406264ba92
alexiscolin <alexis@jaunebleu.co>
2024-11-20 13:21:51 +0900

    chore: remove forum links from nav

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -127,7 +127,6 @@ const links = ref([
   { title: "Twitter", url: "https://twitter.com/_atomone", icon: "twitter" },
   { title: "Discord", url: "https://discord.com/invite/atomone", icon: "discord" },
   { title: "Github", url: "https://github.com/atomone-hub", icon: "github" },
-  { title: "Forum", url: "https://commonwealth.im/govgen", icon: "commonwealth" },
 ]);
 const hasMore = computed(() => {
   return (proposals.value?.proposal_aggregate.aggregate?.count ?? 0) > offset.value + limit.value;
@@ -196,22 +195,33 @@ function setTypeFilterIndex(idx: number) {
           <!-- Chain Title -->
           <!-- Chain Links -->
           <div class="flex flex-row gap-6 text-grey-100 flex-wrap">
-            <a href="https://atom.one" target="_blank" class="flex flex-row gap-2 hover:text-grey-50"
-              @click="logEvent('Click Home Website')">
+            <a
+              href="https://atom.one"
+              target="_blank"
+              class="flex flex-row gap-2 hover:text-grey-50"
+              @click="logEvent('Click Home Website')"
+            >
               <Icon icon="link" /><span>{{ $t("homepage.website") }}</span>
             </a>
             <span>|</span>
             <!-- Chain Socials -->
             <div class="flex flex-row gap-4 items-center justify-center">
-              <a v-for="(linkData, index) in links" :key="index" class="flex items-center" :href="linkData.url"
-                target="_blank" @click="logEvent('Click Home Social', { socialOption: linkData.title })">
+              <a
+                v-for="(linkData, index) in links"
+                :key="index"
+                class="flex items-center"
+                :href="linkData.url"
+                target="_blank"
+                @click="logEvent('Click Home Social', { socialOption: linkData.title })"
+              >
                 <Icon :icon="linkData.icon" class="hover:text-grey-50 hover:cursor-pointer" />
               </a>
             </div>
           </div>
           <!-- Chain Description -->
           <p
-            class="text-grey-100 text-300 lg:w-[656px] font-normal text-left text-pretty after:absolute after:block after:left-0 after:w-full after:bg-grey-300 after:h-px">
+            class="text-grey-100 text-300 lg:w-[656px] font-normal text-left text-pretty after:absolute after:block after:left-0 after:w-full after:bg-grey-300 after:h-px"
+          >
             <i18n-t keypath="homepage.intro" tag="span" class="block pb-8 lg:pb-[72px]">
               <a href="https://commonwealth.im/govgen" class="text-light text-300" target="_blank">{{
                 $t("homepage.forumLinkText")
@@ -221,9 +231,11 @@ function setTypeFilterIndex(idx: number) {
         </div>
       </div>
       <div class="w-1/4">
-        <div v-if="chain_id != 'atomone-1'"
+        <div
+          v-if="chain_id != 'atomone-1'"
           class="flex items-center justify-center gap-4 px-6 py-4 rounded link-gradient text-dark text-center cursor-pointer w-full font-medium"
-          @click="router.push({ path: '/create' })">
+          @click="router.push({ path: '/create' })"
+        >
           <Icon icon="plus" class="font-medium text-400"></Icon>
           <span class="text-300">{{ $t("homepage.createProposal") }}</span>
         </div>
@@ -231,7 +243,8 @@ function setTypeFilterIndex(idx: number) {
     </div>
     <!-- Search Box, and Filters -->
     <div
-      class="flex flex-col w-full justify-start items-center pt-12 lg:pt-[104px] lg:flex-row gap-4 lg:gap-6 lg:justify-between">
+      class="flex flex-col w-full justify-start items-center pt-12 lg:pt-[104px] lg:flex-row gap-4 lg:gap-6 lg:justify-between"
+    >
       <div class="flex flex-col gap-10 w-full lg:flex-row lg:items-center">
         <div class="font-termina text-500 lg:text-600 xl:text-700">{{ $t("homepage.proposalsHeader") }}</div>
         <Search v-model="searchText">
@@ -243,17 +256,24 @@ function setTypeFilterIndex(idx: number) {
         <!-- Select Type -->
         <DropDown v-model="typeFilterIndex" :values="typeFilter" class="w-full lg:w-fit" @select="setTypeFilterIndex" />
         <!-- Show 'x' First -->
-        <DropDown v-model="activityFilterIndex" :values="activityFilter" class="w-full lg:w-fit"
-          @select="setActivityFilterIndex" />
+        <DropDown
+          v-model="activityFilterIndex"
+          :values="activityFilter"
+          class="w-full lg:w-fit"
+          @select="setActivityFilterIndex"
+        />
       </div>
     </div>
     <!-- Proposal View -->
-    <div v-if="proposals"
-      class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 xl:gap-6 mt-[72px]">
+    <div
+      v-if="proposals"
+      class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 xl:gap-6 mt-[72px]"
+    >
       <ProposalCard v-for="proposal in orderedProposals" :key="proposal.id" :link="'/proposals/' + proposal.id">
         <template #header>
           <ProposalStatus
-            :status="PropStatus[(proposal.status ?? 'PROPOSAL_STATUS_UNSPECIFIED') as keyof typeof PropStatus]" />
+            :status="PropStatus[(proposal.status ?? 'PROPOSAL_STATUS_UNSPECIFIED') as keyof typeof PropStatus]"
+          />
         </template>
         <template #number>#{{ proposal.id }}</template>
         <div>{{ proposal.title }}</div>
@@ -280,29 +300,49 @@ function setTypeFilterIndex(idx: number) {
     </div>
 
     <div v-if="proposals" class="flex flex-row justify-end pt-12 gap-4">
-      <Icon icon="Arrowleftend" class="text-400 text-grey-100"
-        :class="{ 'text-light hover:opacity-75 cursor-pointer': offset > 0 }" @click="() => {
-          offset = 0;
-        }
-          " />
-      <Icon icon="Arrowleft" class="text-400 text-grey-100"
-        :class="{ 'text-light hover:opacity-75 cursor-pointer': offset > 0 }" @click="() => {
-          if (offset > 0) {
-            prev();
+      <Icon
+        icon="Arrowleftend"
+        class="text-400 text-grey-100"
+        :class="{ 'text-light hover:opacity-75 cursor-pointer': offset > 0 }"
+        @click="
+          () => {
+            offset = 0;
           }
-        }
-          " />
+        "
+      />
+      <Icon
+        icon="Arrowleft"
+        class="text-400 text-grey-100"
+        :class="{ 'text-light hover:opacity-75 cursor-pointer': offset > 0 }"
+        @click="
+          () => {
+            if (offset > 0) {
+              prev();
+            }
+          }
+        "
+      />
       <!-- Page Numbers -->
-      <Icon icon="Arrowright" class="text-400 text-grey-100"
-        :class="{ 'text-light hover:opacity-75 cursor-pointer': hasMore }" @click="() => {
-          next();
-        }
-          " />
-      <Icon icon="Arrowrightend" class="text-400 text-grey-100"
-        :class="{ 'text-light hover:opacity-75 cursor-pointer': hasMore }" @click="() => {
-          offset = Math.floor((proposals?.proposal_aggregate.aggregate?.count ?? 0) / limit) * limit;
-        }
-          " />
+      <Icon
+        icon="Arrowright"
+        class="text-400 text-grey-100"
+        :class="{ 'text-light hover:opacity-75 cursor-pointer': hasMore }"
+        @click="
+          () => {
+            next();
+          }
+        "
+      />
+      <Icon
+        icon="Arrowrightend"
+        class="text-400 text-grey-100"
+        :class="{ 'text-light hover:opacity-75 cursor-pointer': hasMore }"
+        @click="
+          () => {
+            offset = Math.floor((proposals?.proposal_aggregate.aggregate?.count ?? 0) / limit) * limit;
+          }
+        "
+      />
     </div>
   </div>
 </template>
