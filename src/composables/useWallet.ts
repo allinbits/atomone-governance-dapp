147e8d7dbb4168596509341db1f52a5eafc45ea8
“Clockwork” <alex@clockwork.gr>
2025-10-24 00:24:21 +0300

    chore: raise gas multiplier

--- src/composables/useWallet.ts
+++ src/composables/useWallet.ts
@@ -182,7 +182,7 @@ const useWalletInstance = () => {
           undefined
         );
         const gasLimit = simulate && simulate > 0
-          ? "" + Math.ceil(simulate * 1.5)
+          ? "" + Math.ceil(simulate * 1.7)
           : "500000";
         const result = await client.signAndBroadcast(
           address.value,
